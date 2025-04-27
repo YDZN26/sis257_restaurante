5e2bf28b6424707cac2490640b156c4c27c385fc
unknown <jhojanaldairs@gmail.com>
2025-04-26 20:25:26 -0400

    modulos

--- backend_restaurante/src/app.module.ts
+++ backend_restaurante/src/app.module.ts
@@ -1,9 +1,17 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsuarioModule } from './usuario/usuario.module';
+import { PlatillosModule } from './platillos/platillos.module';
+import { RepartidorModule } from './repartidor/repartidor.module';
+import { PedidosModule } from './pedidos/pedidos.module';
+import { PagoModule } from './pago/pago.module';
+import { DetallesModule } from './detalles/detalles.module';
+import { DireccionesModule } from './direcciones/direcciones.module';
+import { ClienteModule } from './cliente/cliente.module';
 
 @Module({
-  imports: [],
+  imports: [UsuarioModule, PlatillosModule, RepartidorModule, PedidosModule, PagoModule, DetallesModule, DireccionesModule, ClienteModule],
   controllers: [AppController],
   providers: [AppService],
 })
