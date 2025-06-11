bd7685e0f362147280c1e21819bd3e622fe59984
amanda176 <mandaserrudo60@gmail.com>
2025-06-11 19:55:10 -0400

    CRUD DE FRONTEND

--- backend_restaurant/src/app.module.ts
+++ backend_restaurant/src/app.module.ts
@@ -8,16 +8,15 @@ import { PedidoModule } from './pedidos/pedidos.module';
 console.log('DB_HOST:', process.env.DB_HOST);
 @Module({
   imports: [
-    ConfigModule.forRoot({
-      isGlobal: true,
-    }),
+    ConfigModule.forRoot(),
     TypeOrmModule.forRoot({
       type: 'postgres',
       host: process.env.DB_HOST,
-      port: parseInt(process.env.DB_PORT || '5432', 10),
+      port: Number(process.env.DB_PORT),
       username: process.env.DB_USERNAME,
       password: process.env.DB_PASSWORD,
       database: process.env.DB_NAME,
+      entities: [__dirname + '*/**/entities/*.{ts|js}'],
       autoLoadEntities: true,
       synchronize: true,
     }),
