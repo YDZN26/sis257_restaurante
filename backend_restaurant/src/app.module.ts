55051f0a097f3ae959fbb8dc0b76a83633a10f70
YDZN26 <125135943+YDZN26@users.noreply.github.com>
2025-06-26 16:27:12 -0400

    Corrige puerto para despliegue en Render 2

--- backend_restaurant/src/app.module.ts
+++ backend_restaurant/src/app.module.ts
@@ -18,15 +18,14 @@ console.log('DB_HOST:', process.env.DB_HOST);
     ConfigModule.forRoot(),
     TypeOrmModule.forRoot({
       type: 'postgres',
-      host: process.env.DB_HOST,
-      port: Number(process.env.DB_PORT),
-      username: process.env.DB_USERNAME,
-      password: process.env.DB_PASSWORD,
-      database: process.env.DB_NAME,
-      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      url: process.env.DATABASE_URL,
       autoLoadEntities: true,
       synchronize: true,
+      ssl: {
+        rejectUnauthorized: false,
+      },
     }),
+
     PlatillosModule,
     AuthModule,
     CategoriaPlatillosModule,
